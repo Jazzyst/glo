dd8531a43a504a23f6e224df6d90dd370b76789e
Jazzyst <andriy.zhuravlov@gmail.com>
2021-01-31 17:25:43 +0200

    homework02

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,20 @@
-let money, income, addExpenses, deposit, missing, period;
-console.log('some text');
-alert('some text')
+let money, income, addExpenses, deposit, mission, period, splittedStr, budgetDay;
+
+money = 2000;
+income = 'Freelance income';
+addExpenses = 'internet, taxi, food, entertainment, other';
+deposit = true;
+mission = 100000;
+period = 12;
+
+console.log( 'money:' + typeof money, 'income:' + typeof income, 'deposit:' + typeof deposit);
+console.log('AddExpenses: ', addExpenses.length);
+console.log(`Период равен ${period} месяцев`);
+console.log(`Цель заработать ${mission} долларов`);
+
+splittedStr = addExpenses.toLowerCase().split(',');
+console.log(splittedStr);
+
+budgetDay = money / 30;
+console.log('budgetDay', budgetDay);
+
