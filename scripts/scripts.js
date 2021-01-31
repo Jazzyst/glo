709d727f34a23de4054d3072997483e045db939b
Jazzyst <andriy.zhuravlov@gmail.com>
2021-01-31 18:12:12 +0200

    hardtask01

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,9 @@
-let money, income, addExpenses, deposit, missing, period;
-console.log('some text');
-alert('some text')
+let num = 266219;
+
+let result = num.toString().split('').reduce(( res, el) => el * res, 1 );
+console.log(result);
+
+result **= 3;
+result = result.toString().substr(0, 2)
+console.log(result);
+
