d832c40c072cb5ac966b9e2b1d548c15a0c92527
Jazzyst <andriy.zhuravlov@gmail.com>
2021-01-31 18:17:05 +0200

    hardtask02

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -4,6 +4,6 @@ let result = num.toString().split('').reduce(( res, el) => el * res, 1 );
 console.log(result);
 
 result **= 3;
-result = result.toString().substr(0, 2)
+result = result.toString().substr(0, 2);
 console.log(result);
 
