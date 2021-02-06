dccf7eb65bc3813467da9b3a84112581ea47f20f
Jazzyst <andriy.zhuravlov@gmail.com>
2021-02-06 22:27:48 +0200

    hw6

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,93 +1,32 @@
-let money, income, addExpenses, deposit, mission, period, splittedStr, budgetDay, expenses, missionComplited;
 
-const isNumber = (n) => {
-  return !isNaN(parseFloat(n)) && isFinite(n)
-};
+let randomNumber = Math.floor(1 + Math.random() * (100 + 1 - 1));
 
-income = 'Freelance income';
-addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую', 'Квартплата, проездной, кредит');
-deposit = confirm('Есть ли у вас депозит в банке?');
-mission = 100000;
-period = 12;
-
-const start = () => {
-  do {
-    money = prompt('Ваш месячный доход?');
-  } while (!isNumber(money))
-};
-start();
-
-const showTypeOf = (data) => {
-  console.log(data, typeof (data))
-}
-showTypeOf(money);
-showTypeOf(income);
-showTypeOf(deposit);
-
-expenses = [];
-splittedStr = addExpenses.toLowerCase().split(', ');
-
-const getExpensesMonth = () => {
-  let sum;
-
-  for (let i = 0; i < 2; i++) {
-    expenses[i] = prompt(`Введите обязательную статью расходов №${i + 1}?`);
-    do {
-      sum = prompt(`Во сколько это обойдется?`);
-    } while (!isNumber(sum))
-    sum = +sum + +sum;
+const getStatusIncome = (userNumber) => {
+  userNumber = prompt('Угадай число от 1 до 100');
+  console.log(userNumber, randomNumber)
+  if(userNumber === null){
+    alert('Игра окончена');
+    return;
   }
-  console.log(expenses);
-  return sum;
-};
 
-let expensesAmount = getExpensesMonth()
-
-console.log('Расходы за месяц: ', expensesAmount);
-console.log('Возможные расходы: ', splittedStr);
-
-const getAccumulatedMonth = (monthIncome) => {
-  return monthIncome - expensesAmount;
-}
-
-const accumulatedMonth = getAccumulatedMonth(money);
-console.log('accumulatedMonth', accumulatedMonth);
-
-const getTargetMonth = (mission, accumMonth) => {
-  if (accumMonth > 0) {
-    missionComplited = Math.ceil(mission / accumMonth);
-    return `Цель будет достигнута через ${missionComplited} месяцев(-а)`;
-  }
-  return `Цель не будет достигнута, у Вас нет доходов`;
-}
-console.log(getTargetMonth(mission, accumulatedMonth))
-
-budgetDay = Math.floor(accumulatedMonth / 30);
-console.log('Бюджет на день: ', budgetDay);
-
-const getStatusIncome = () => {
   switch (true) {
-    case budgetDay >= 1200:
-      return 'У вас высокий уровень дохода';
+    case userNumber > randomNumber:
+      alert('Загаданное число меньше');
+      getStatusIncome(userNumber)
       break;
-    case budgetDay >= 600 && budgetDay < 1200:
-      return 'У вас средний уровень дохода';
+    case userNumber < randomNumber:
+      alert('Загаданное число больше');
+      getStatusIncome(userNumber)
       break;
-    case budgetDay <= 600 && budgetDay >= 0:
-      return 'К сожалению у вас уровень дохода ниже среднего';
+    case isNaN(userNumber):
+      alert('Введи число!');
+      getStatusIncome(userNumber)
       break;
-    case budgetDay < 0:
-      return 'Что то пошло не так';
+    case userNumber == randomNumber:
+      alert('Поздравляю, Вы угадали!!!');
       break;
-    default:
-      return 'Я еще не видел таких денег';
   }
 }
-console.log(getStatusIncome());
-
-
-
-
-
+getStatusIncome();
 
 
